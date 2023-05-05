37b294ca88cd41cfd1de2fdd803183171c7093ce
A Ridley <expireddata@gmail.com>
2023-05-05 15:30:31 +0100

    feat: add query to fetch list of contacts

---
+++ queries/Contacts.test.tsx
@@ -0,0 +1,39 @@
+import { useGetContacts } from "./Contacts";
+import { act, render, waitFor } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockContact = {
+  createdAt: "2023-02-12T13:12:08.303Z vvvv",
+  name: "Test Name",
+  avatar:
+    "https://images.pexels.com/photos/1996333/pexels-photo-1996333.jpeg?cs=srgb&dl=pexels-helena-lopes-1996333.jpg&fm=jpg",
+  email: "test@example.com",
+  phone: "89908908909889089",
+  birthday: "01/01/2001",
+  id: "133",
+};
+
+global.fetch = jest.fn(
+  () =>
+    Promise.resolve({
+      json: Promise.resolve([mockContact]),
+    }) as unknown as Promise<Response>
+);
+
+const TestComponent = () => {
+  const { isLoading, contacts } = useGetContacts();
+
+  return <>{isLoading ? "Loading" : contacts![0].name}</>;
+};
+
+test("get contacts contains contacts", async () => {
+  const queryClient = new QueryClient();
+  const { getByText } = render(
+    <QueryClientProvider client={queryClient}>
+      <TestComponent />
+    </QueryClientProvider>
+  );
+  await waitFor(() => expect(() => getByText("Tfdst Name")).toBeDefined(), {
+    timeout: 5000,
+  });
+});
---
+++ queries/Contacts.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "react-query";
+
+const baseUrl = "https://61c32f169cfb8f0017a3e9f4.mockapi.io/";
+
+type Contact = {
+  avatar: string;
+  birthday: string;
+  createdAt: string;
+  email: string;
+  id: string;
+  name: string;
+  phone: string;
+};
+
+type GetContactResponse = {
+  isLoading: boolean;
+  error: unknown;
+  contacts: Contact[] | undefined;
+};
+
+const fetchContacts = () =>
+  fetch(`${baseUrl}/api/v1/contacts`).then(req => req.json());
+
+export const useGetContacts = (): GetContactResponse => {
+  const { isLoading, error, data } = useQuery<Contact[]>("contacts", fetchContacts);
+
+  return { isLoading, error, contacts: data };
+};
